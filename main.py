c0b3b4667bb29c96985f1b93148d8cc71555a4a9
daianedeise <daianeferreiraheitor@gmail.com>
2025-10-08 16:19:19 -0300

    Subindo Projeto Sprint 7

--- main.py
+++ main.py
@@ -1,10 +1,59 @@
+import data
+import helpers
 
-def in_autotests_we_trust(a, b):
-    if a == b:
-        print('PASS')
-    else:
-        print('FAIL')
 
-in_autotests_we_trust(10, '10')
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
 
-in_autotests_we_trust(0, False)
+    def test_set_route(self):
+        # Adicionar em S8
+        print("Função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("Função criada para definir plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("Função criada para fill phone number")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("Função criada para fill card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("Função criada para comment for driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("Função criada para definir order blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        print("Função criada para definir order 2 ice creams")
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            # Adicionar em S8
+            print("Função criada para order de sorvete")
+            pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("Função criada para definir car search model appears")
+        pass
+
+
+
+
