64a0bbebab88b24b9619f670f11ba028b04bd174
daianedeise <daianeferreiraheitor@gmail.com>
2025-10-25 11:14:37 -0300

    Subindo Projeto Sprint 7

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
-URBAN_ROUTES_URL = 'https://cnt-1d453f1c-2f4e-473d-91cb-b6d9baf61140.containerhub.tripleten-services.com?lng=pt'
+URBAN_ROUTES_URL = 'https://cnt-3fd26e38-7b36-43c4-94d4-1660b2e7afa4.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
 CARD_NUMBER = '1234 5678 9100'
 CARD_CODE = '1111'
 MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
+
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import data
 import helpers
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+from pages import UrbanRoutesPage
+from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import WebDriverWait
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        cls.driver = Chrome()
+        cls.driver.implicitly_wait(5)
+
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("Conectado ao servidor Urban Routes")
         else:
             print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
 
     def test_set_route(self):
-        # Adicionar em S8
-        print("Função criada para definir a rota")
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert routes_page.get_from_location_value() == data.ADDRESS_FROM
+        assert routes_page.get_to_location_value() == data.ADDRESS_TO
+
+
 
     def test_select_plan(self):
         # Adicionar em S8
@@ -51,6 +67,10 @@ class TestUrbanRoutes:
         # Adicionar em S8
         print("Função criada para definir car search model appears")
         pass
+
+        @classmethod
+        def teardown_class(cls):
+            cls.driver.quit()
 
 
 
